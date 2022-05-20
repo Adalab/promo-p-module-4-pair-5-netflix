e65ee3791798ea4333d46e3d9d7dfaee0283ae75
andalons <andreacor001@gmail.com>
2022-05-20 18:07:26 +0200

    set up workspace to study

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const Database = require('better-sqlite3');
 const server = express();
 server.use(cors());
 server.use(express.json());
+
+// set up EJS (template engine/motor de plantillas) to create dinamic server. Also necessary to create views/ folder to create the templates (movie.ejs in this case).
 server.set('view engine', 'ejs');
 
 // data from .json (first exercises)
@@ -15,44 +17,29 @@ const movies = require('./data/movies.json');
 // data (from database)
 const db = new Database('./src/db/database.db', { verbose: console.log });
 
-// init express aplication
+// set up port and start the server (express application)
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
-//Endpoints:
+//ENDPOINTS:
 
+//this endpoint listens to the changes in URL params. IMPORTANT to set it before static servers. otherwise won't work.
+//it is not related to a fetch in front-end. to see each movie, go to the route http://localhost:4000/movie/1 (or 2, 3, 4)
 server.get('/movie/:movieId', (req, res) => {
   console.log(req.params);
-
   const foundMovie = movies.find(
     (eachMovie) => eachMovie.id === req.params.movieId
   );
-  res.render('movie', foundMovie);
+  res.render(
+    'movie' /* route to the specific template inside views */,
+    foundMovie
+  );
   console.log(foundMovie);
 });
 
-// creamos el endpoint /users de tipo GET
-
-//End points:
-//este recibe la petición del fetch de api-movies
-server.get('/movies', (req, res) => {
-  const sort = req.query.sort;
-  if (req.query.gender !== '') {
-    const query = db.prepare(
-      `SELECT * FROM movies WHERE gender = ? ORDER BY name ${sort}`
-    );
-    const movies = query.all(req.query.gender);
-    res.json(movies);
-  } else {
-    const query = db.prepare(`SELECT * FROM movies ORDER BY name ${sort}`);
-    const movies = query.all();
-    res.json(movies);
-  }
-});
-
-/* cuando los datos los cogía de un json en lugar de la base de datos
+/* at first, when data was stored in json instead of database, this was the endpoint to get, filter and sort the movies shown:
 
   server.get('/movies', (req, res) => {
   const response = movies;
@@ -92,7 +79,23 @@ server.get('/movies', (req, res) => {
   );
 }); */
 
-//este recibe la petición del fetch de api-user
+//this endpoint receives parameters from fetch in web/src/services/api-movies
+server.get('/movies', (req, res) => {
+  const sort = req.query.sort;
+  if (req.query.gender !== '') {
+    const query = db.prepare(
+      `SELECT * FROM movies WHERE gender = ? ORDER BY name ${sort}`
+    );
+    const movies = query.all(req.query.gender);
+    res.json(movies);
+  } else {
+    const query = db.prepare(`SELECT * FROM movies ORDER BY name ${sort}`);
+    const movies = query.all();
+    res.json(movies);
+  }
+});
+
+//receives parameters from fetch in web/src/services/api-user (sendLoginToApi)
 server.post('/login', (req, res) => {
   console.log(req.body);
   const userFound = users.find(
@@ -131,7 +134,7 @@ server.post('/sign-up', (req, res) => {
   }
 });
 
-//static routes:
+//static servers set up:
 const staticServerPathWeb = './src/public-react';
 server.use(express.static(staticServerPathWeb));
 
